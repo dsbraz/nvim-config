c2f14a001060af424baa2c2621c19f69c89c6efd
Daniel Braz <dsbraz@dsbraz.com>
2026-06-13 01:17:05 -0300

    Keep window open when closing buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,33 @@
-vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete, { desc = "Close buffer" })
+local function close_buffer_keep_window()
+	local current = vim.api.nvim_get_current_buf()
+	local alternate = vim.fn.bufnr("#")
+
+	if alternate ~= current and vim.fn.buflisted(alternate) == 1 then
+		vim.cmd.buffer(alternate)
+	else
+		local target
+
+		for _, buffer in ipairs(vim.fn.getbufinfo({ buflisted = 1 })) do
+			if buffer.bufnr ~= current then
+				target = buffer.bufnr
+				break
+			end
+		end
+
+		if target then
+			vim.cmd.buffer(target)
+		else
+			vim.cmd.enew()
+		end
+	end
+
+	local ok = pcall(vim.cmd, "bdelete " .. current)
+	if not ok and vim.api.nvim_buf_is_valid(current) then
+		vim.cmd.buffer(current)
+	end
+end
+
+vim.keymap.set("n", "<leader>bd", close_buffer_keep_window, { desc = "Close buffer" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
